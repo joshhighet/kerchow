8ef9a93e8f3a93516627476ff5835aadb492a919
josh! <github@joshhighet.com>
2023-01-05 10:22:22 +1300

    Update getsplunkent.py

--- squipts/getsplunkent.py
+++ squipts/getsplunkent.py
@@ -2,12 +2,14 @@
 '''
 return download links for the latest version of Splunk Enterprise
 '''
-import logging
-import requests
 try:
+    import sys
+    import logging
+    import requests
+    import platform
     from bs4 import BeautifulSoup
-except ImportError:
-    logging.error('beautifulsoup is required and missing - refer to https://pypi.org/project/beautifulsoup4')
+except ImportError as e:
+    logging.error('missing module: %s', e)
     exit(1)
 releases_data = requests.get(
     'https://www.splunk.com/en_us/download/previous-releases.html',
@@ -28,16 +30,32 @@ for release_url_segment in release_url_segments:
         release_urls_osx.append(release_url_segment['data-link'])
     elif release_url_segment['data-platform'] == 'windows':
         release_urls_windows.append(release_url_segment['data-link'])
-# each release_urls_* list contains all available releases for a given platform
 latest_version_linux = release_urls_linux[0].split('/')[6]
-for release_url_linux in release_urls_linux:
-    if latest_version_linux in release_url_linux:
-        print(release_url_linux)
 latest_version_osx = release_urls_osx[0].split('/')[6]
-for release_url_osx in release_urls_osx:
-    if latest_version_osx in release_url_osx:
-        print(release_url_osx)
 latest_version_windows = release_urls_windows[0].split('/')[6]
-for release_url_windows in release_urls_windows:
-    if latest_version_windows in release_url_windows:
-        print(release_url_windows)
+if len(sys.argv) > 1 and sys.argv[1] == '--all':
+    for release_url_linux in release_urls_linux:
+        if latest_version_linux in release_url_linux:
+            print(release_url_linux)
+    for release_url_osx in release_urls_osx:
+        if latest_version_osx in release_url_osx:
+            print(release_url_osx)
+    for release_url_windows in release_urls_windows:
+        if latest_version_windows in release_url_windows:
+            print(release_url_windows)
+else:
+    current_system = platform.system().lower()
+    if current_system == 'linux':
+        for release_url_linux in release_urls_linux:
+            if latest_version_linux in release_url_linux:
+                print(release_url_linux)
+    elif current_system == 'darwin':
+        for release_url_osx in release_urls_osx:
+            if latest_version_osx in release_url_osx:
+                print(release_url_osx)
+            elif current_system == 'windows':
+                for release_url_windows in release_urls_windows:
+                    if latest_version_windows in release_url_windows:
+                        print(release_url_windows)
+    else:
+        print(f'unsupported platform: {current_system}')
