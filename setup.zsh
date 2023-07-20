51e8e2714c7cb8c0d339d3a4f782a5c5d03d69ee
josh! <github@joshhighet.com>
2023-07-21 09:43:36 +1200

    sh/zsh

--- setup.zsh
+++ setup.zsh
@@ -5,8 +5,13 @@ set -e #-x
 
 location=`timeout .1 git rev-parse --show-toplevel`
 
+if [[ ! "${SHELL}" =~ "zsh" ]] ; then
+  echo "setup needs to be executed from within zsh"
+  exit 1
+fi
+
 if [[ ! "${location}" =~ "kerchow" ]] ; then
-  echo "paths needs to be executed from within joshhighet/kerchow"
+  echo "setup needs to be executed from within joshhighet/kerchow"
   exit 1
 fi
 
@@ -15,7 +20,7 @@ if ! grep "kerchow/sbin" ~/.zshrc &>/dev/null ; then
     cp ~/.zshrc ~/.zshrc-backup-joshhighet-kerchow-sbin
     echo "kerchow/sbin not in path, adding"
     echo "" >> ~/.zshrc
-    echo "# added by sbin/paths from joshhighet/kerchow" >> ~/.zshrc
+    echo "# added by setup.zsh from joshhighet/kerchow" >> ~/.zshrc
     echo "export PATH=${location}/sbin:\$PATH" >> ~/.zshrc
     echo "added, reopen your terminal or introduce into current session with 'source ~/.zshrc'"
 else
